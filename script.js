52574392a012517f1700e77901f9ac47baf0d556
Merior94 <45514222+Merior94@users.noreply.github.com>
2022-12-08 22:25:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://93.180.179.200:8080/v1/tasks';
+  var apiRoot = 'https://93.180.179.200:8080/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
